820590b7962f4749710de7bce8f17abc3d6ecee3
Sohaib-Ahmed869 <i211114@nu.edu.pk>
2024-05-17 19:53:10 +0500

    hi

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/ecommerce001').then(() => {
+mongoose.connect('mongodb+srv://sohaibsipra869:nvidia940MX@cluster0.dcrk8mp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {
     console.log('Connected to MongoDB');
     }
 )
